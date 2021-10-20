252c9db4a4aecc395aa9a9a3b12aed0d547bbc68
paul-coder-22 <kironpaul889@gmail.com>
2021-10-20 14:47:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-form-validation-eg-kemxvj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-form-validation-eg-kemxvj)
---
+++ app/input-error-state-matcher-example.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** @title Input with a custom ErrorStateMatcher */
+@Component({
+  selector: 'input-error-state-matcher-example',
+  templateUrl: './input-error-state-matcher-example.html',
+  styleUrls: ['./input-error-state-matcher-example.css'],
+})
+export class InputErrorStateMatcherExample {
+  userAddressValidations: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit() {
+    this.userAddressValidations = this.formBuilder.group({
+      mobileNumber: [
+        '',
+        [Validators.required, Validators.pattern('[6-9]\\d{9}')],
+      ],
+    });
+  }
+  _keyPress(event: any) {
+    const pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+}
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
